3c82439a4131cfde325efa5e0ee42cf4e8fd00c0
EliDoris <76080074+EliDoris@users.noreply.github.com>
2021-03-19 22:49:47 -0400

    Added preferences for external song file sourcing

--- app/src/main/java/com/example/groupsongviewer/SettingsFragment.kt
+++ app/src/main/java/com/example/groupsongviewer/SettingsFragment.kt
@@ -1,15 +1,22 @@
 package com.example.groupsongviewer
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.text.InputType
 import android.widget.Toast
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
+
+//Identifier for return value from intent
+const val EXTRA_DOCTREE = 101
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
@@ -28,16 +35,57 @@ class SettingsFragment : PreferenceFragmentCompat() {
         //Reset handling
         val resetPrefButton: Preference? = findPreference("reset_preferences")
         resetPrefButton?.setOnPreferenceClickListener {
+            //Load preferences and reset
             val prefs = PreferenceManager.getDefaultSharedPreferences(this.context)
-            Toast.makeText(this.context, "Preferences Reset", Toast.LENGTH_SHORT).show()
             prefs.edit().clear().commit()
+            //Notify the user
+            Toast.makeText(this.context, "Preferences Reset", Toast.LENGTH_SHORT).show()
             //Reload the fragment
             val sfm = requireActivity().supportFragmentManager
             sfm.beginTransaction().replace(R.id.settings_fragment,SettingsFragment()).commit()
             //RETURN VALUES IN LAMBDA FUNCTIONS YA GOOF
             true
         }
+
+        //External file location handling
+        val externalLocationButton: Preference? = findPreference("external_song_file_location")
+        externalLocationButton?.setOnPreferenceClickListener {
+            //Load preferences and get URI from the preference string
+            val prefs = PreferenceManager.getDefaultSharedPreferences(this.context)
+            val uri: Uri = prefs.getString("external_song_file_location","/")?.toUri()!!
+            //Do the opening of the directory
+            openDirectory(uri)
+            //Obligatory true return
+            true
+        }
+        val externalLocationPreference: Preference? = findPreference("external_song_file_location")
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this.context)
+        externalLocationPreference?.summary = prefs.getString("external_song_file_location","")
+
     }
 
+    //Function for opening a document location
+    private fun openDirectory(pickerInitialUri: Uri) {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply{
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+        startActivityForResult(intent, EXTRA_DOCTREE)
+    }
+    //Function for handling a received activity result. Currently only handles file location
+    @SuppressLint("ApplySharedPref")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        //Verify the right code
+        if (requestCode == EXTRA_DOCTREE) {
+            //Use the result to update preferences
+            resultData?.data?.also {uri ->
+                //Set preference
+                val prefs = PreferenceManager.getDefaultSharedPreferences(this.context)
+                prefs.edit().putString("external_song_file_location",uri.toString()).commit()
+                //Update summary
+                val externalLocationPreference: Preference? = findPreference("external_song_file_location")
+                externalLocationPreference?.summary = uri.toString()
+            }
+        }
+    }
 
 }
